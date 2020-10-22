bd66e40cc215e1da556a747e19c3aa7d3c62d770
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-22 10:59:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,9 +8,7 @@ class App < Sinatra::Base
 
   # Add your post route and action below
   post '/food' do
-    post '/food' do
-        params.to_s
-      end
-  end
+      params.to_s
+    end
 
 end
